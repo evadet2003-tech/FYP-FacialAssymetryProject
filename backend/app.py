3772d9151ff84598d6f136c065ef46cdf04826dc
evadet2003-tech <evadet2003@gmail.com>
2026-06-22 23:45:34 +0100

    Fix CORS allow all origins

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173", "https://fyp-facialassymetryproject-production.up.railway.app"])
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
